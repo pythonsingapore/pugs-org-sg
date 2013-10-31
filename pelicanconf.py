31df8f679602bb4deb60c459a03395ef74a005fb
Ivan <vanzaj@gmail.com>
2013-10-31 20:59:32 +0800

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ivan'
+SITENAME = u'pugs.org.sg'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Singapore'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('python.org', 'http://python.org/'),
+          ('pycon.org', 'http://pycon.org/'),
+          ('pycon.sg', 'http://pycon.sg/'),)
+
+# Social widget
+SOCIAL = (('Facebook', 'https://www.facebook.com/pythonsg'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
